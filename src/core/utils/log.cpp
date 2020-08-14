d9242e25d5081afd3c0d2da25ce7a86bf65f6f22
Simon Spannagel <simon.spannagel@cern.ch>
2020-08-14 18:13:29 +0200

    Logger: improve style of prgress bar

--- src/core/utils/log.cpp
+++ src/core/utils/log.cpp
@@ -260,29 +260,30 @@ void DefaultLogger::drawProgressBar(std::string identifier,
                                     uint32_t line) {
     // Get a progress stream, set indicator to "#" to indicate progress bar (is overwritten)
     std::ostringstream& stream = getProcessStream(identifier, level, file, function, line);
+    stream << " " << std::setw(3) << std::setfill(' ') << (100 * current) / total << "% ";
 
     std::string events = " " + std::to_string(current) + " / " + std::to_string(total) + " ";
     static std::array<std::string, 8> partial_block{
         " ", "\u258F", "\u258E", "\u258D", "\u258C", "\u258B", "\u258A", "\u2589"};
 
-    auto width = static_cast<uint64_t>(query_line_length() - stream.tellp());
+    auto width = query_line_length() - indent_count_ - 10;
     auto completed = (width * current) / total;
-    auto pending = completed - 1;
-
+    auto pending = (completed >= width ? 0ul : width - completed - 1);
     auto remainder = (8 * current * width / total) % 8;
 
-    stream << " " << std::setw(3) << std::setfill(' ') << (100 * current) / total << "% ";
-
-    if(events.length() <= pending) {
+    if(events.length() < pending) {
         stream << "\x1B[7m" << std::string(completed, ' ') << "\x1B[0m";
         stream << partial_block.at(remainder) << events;
         stream << std::string(pending - events.length(), ' ');
-    } else {
+    } else if(events.length() < completed) {
         stream << "\x1B[7m" << std::string(completed - events.length(), ' ');
         stream << events << "\x1B[0m" << partial_block.at(remainder);
         stream << std::string(pending, ' ');
+    } else {
+        stream << "\x1B[7m" << std::string(completed, ' ');
+        stream << "\x1B[0m" << partial_block.at(remainder);
+        stream << std::string(pending, ' ');
     }
-    stream << '|';
 }
 
 // Getter and setters for the reporting level
