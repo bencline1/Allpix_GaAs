3e775c7010f1371950720f685b90feebfa686bbb
Simon Spannagel <simon.spannagel@cern.ch>
2021-02-23 07:56:21 +0100

    DepositionBichsel: add headers

--- src/modules/DepositionBichsel/MazziottaIonizer.cpp
+++ src/modules/DepositionBichsel/MazziottaIonizer.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file
+ * @brief Implementation of ionization and shell transitions
+ * @remarks Based on code from Daniel Pitzel and - in turn - H. Bichsel and M. Mazziotta.
+ * @copyright Copyright (c) 2021 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
 #include "MazziottaIonizer.hpp"
 
 #include "core/utils/log.h"
--- src/modules/DepositionBichsel/MazziottaIonizer.hpp
+++ src/modules/DepositionBichsel/MazziottaIonizer.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file
+ * @brief Definition of shell ionization mechanisms following Mazziotta
+ * @copyright Copyright (c) 2021 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
 #include <random>
 #include <stack>
 
